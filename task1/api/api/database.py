064152de946812a1af6d2d814082c01130439286
koralexa <koralexa@ispras.ru>
2024-03-19 12:30:40 +0300

    Add compose and readme

--- task1/api/api/database.py
+++ task1/api/api/database.py
@@ -9,7 +9,11 @@ mysql_host = os.environ.get("MYSQL_HOST")
 mysql_port = os.environ.get("MYSQL_PORT")
 mysql_db_name = os.environ.get("MYSQL_DB_NAME")
 
+print(mysql_user, mysql_password, mysql_host, mysql_port, mysql_db_name)
+
 MYSQL_DATABASE_URL = f"mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_host}:{mysql_port}/{mysql_db_name}"
+
+print(MYSQL_DATABASE_URL)
 
 engine: Engine = create_engine(MYSQL_DATABASE_URL, isolation_level="SERIALIZABLE")
 SessionLocal: sessionmaker[Session] = sessionmaker(autocommit=False, autoflush=False, bind=engine)
